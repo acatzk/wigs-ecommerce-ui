c97c1922cb1daf2f0c04daac76384deee8ead6f5
acatzk <joshuaimalay@gmail.com>
2021-07-21 12:20:13 +0800

    ✨ FEATURE: Add wigs content promotion

--- src/layouts/defaultLayout.tsx
+++ src/layouts/defaultLayout.tsx
@@ -5,16 +5,16 @@ import Sidebar from '~/components/Sidebar'
 const defaultLayout: React.FC = ({ children }) => {
   return (
     <div className="relative antialiased min-h-screen bg-[#E5E5E5] overflow-x-hidden">
-      <div className="absolute w-[355px] h-[355px] lg:scale-75 top-16 left-[29px] bg-[#9872FF] opacity-10 rounded-full mix-blend-multiply blur-3xl"></div>
+      {/* <div className="absolute w-[355px] h-[355px] lg:scale-75 top-16 left-[29px] bg-[#9872FF] opacity-10 rounded-full mix-blend-multiply blur-3xl"></div>
       <div className="absolute w-[534px] h-[244px] lg:scale-75 bottom-0 left-[80px] bg-[#5D9CE6] opacity-40 rounded-full mix-blend-multiply blur-3xl"></div>
       <div className="absolute w-[495px] h-[495px] lg:scale-75 right-0 bg-[#3A09BC] opacity-20 rounded-full mix-blend-multiply blur-3xl"></div>
       <div className="absolute w-[516px] h-[341px] lg:scale-75 bottom-0 right-0 bg-[#FE5C31] opacity-20 rounded-full mix-blend-multiply blur-3xl"></div>
       <div className="flex justify-center">
         <div className="absolute w-[874px] h-[874px] flex items-center justify-center bg-[#9F7FE8] opacity-30 rounded-full mix-blend-multiply blur-3xl"></div>
-      </div>
+      </div> */}
       <main className="flex bg-white/70 backdrop-blur-md">
         <Sidebar />
-        <section className="flex-grow">
+        <section className="flex-1 h-full w-full">
           <Header />
           {children}
         </section>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
+import Image from 'next/image'
 import { NextPage } from 'next'
 import Layout from '~/layouts/defaultLayout'
 
@@ -10,9 +11,72 @@ const Index: NextPage = () => {
         <title>Wigs Ecommerce UI</title>
       </Head>
       <Layout>
-        <div className="font-bold text-2xl text-purple-600">Hi Gayle</div>
+        <div
+          className="grid grid-cols-2 gap-4 px-4 py-4"
+          style={{ height: '88vh' }}
+        >
+          <div className="grid gap-4">
+            <div className="grid gap-4">
+              <ImageContent src="/svgs/content/promo.svg" />
+              <ImageContent src="/svgs/content/summer.svg" />
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <ImageContent src="/svgs/content/women1.svg" heart />
+              <ImageContent src="/svgs/content/women2.svg" heart />
+            </div>
+          </div>
+          <div className="grid gap-4">
+            <div className="grid grid-cols-2 gap-4">
+              <ImageContent src="/svgs/content/women3.svg" heart />
+              <ImageContent src="/svgs/content/women4.svg" heart />
+            </div>
+            <div className="grid gap-4">
+              <ImageContent src="/svgs/content/women5.svg" />
+              <ImageContent src="/svgs/content/women6.svg" />
+            </div>
+          </div>
+        </div>
       </Layout>
     </React.Fragment>
+  )
+}
+
+const ImageContent: React.FC<{ src: string; heart?: boolean | false }> = ({
+  src,
+  heart,
+}) => {
+  return (
+    <div className="relative rounded-2xl overflow-hidden">
+      {heart && (
+        <button
+          className="absolute z-30 top-5 right-6 
+        flex items-center justify-between p-2 rounded-full bg-white group 
+        hover:bg-red-500 shadow-sm transition ease-in-out duration-150"
+        >
+          <svg
+            className="w-5 h-5 group-hover:text-white transition ease-in-out duration-150"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            color="#000000"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1"
+              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+            ></path>
+          </svg>
+        </button>
+      )}
+      <Image
+        src={src}
+        layout="fill"
+        className="w-full h-full object-cover bg-gray-200"
+        alt="promo"
+      />
+    </div>
   )
 }
 
