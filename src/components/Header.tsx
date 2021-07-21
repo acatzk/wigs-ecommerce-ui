65a61edf5adf0bdf3c1757bd9dc22f374be2d1bf
acatzk <joshuaimalay@gmail.com>
2021-07-21 20:11:56 +0800

    📱 RES: Improve mobile responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,27 +11,47 @@ const Header: React.FC = () => {
         <input
           type="text"
           placeholder="Search store"
-          className="focus:outline-none bg-transparent text-sm"
+          className="hidden md:block focus:outline-none bg-transparent text-sm"
         />
       </div>
-      <nav className="mr-10 flex items-center text-black text-sm font-semibold space-x-12">
-        <ul className="flex items-center space-x-10">
-          {navbarLinks.map(({ title }, i) => (
-            <li key={i}>
-              <a
-                href="#"
-                className={`${
-                  title === 'products' &&
-                  'border-b-2 border-black pb-2 hover:border-gray-600'
-                } 
-                    hover:text-gray-600 transition ease-in-out duration-150`}
-              >
-                {title}
-              </a>
-            </li>
-          ))}
-        </ul>
+      <nav className="mr-0 md:mr-10 flex items-center text-black text-sm font-semibold space-x-12">
+        <div className="hidden md:block">
+          <ul className="flex items-center space-x-10">
+            {navbarLinks.map(({ title }, i) => (
+              <li key={i}>
+                <a
+                  href="#"
+                  className={`${
+                    title === 'products' &&
+                    'border-b-2 border-black pb-2 hover:border-gray-600'
+                  } 
+                  hover:text-gray-600 transition ease-in-out duration-150`}
+                >
+                  {title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
         <ul className="flex items-center space-x-6">
+          <li className="block md:hidden">
+            <button className="text-black hover:text-gray-600 transition ease-in-out duration-150">
+              <svg
+                className="w-6 h-6 text-black"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                ></path>
+              </svg>
+            </button>
+          </li>
           <li>
             <button className="relative w-6 h-6 hover:text-gray-600 transition ease-in-out duration-150">
               <Image src="/svgs/trash.svg" layout="fill" alt="trash-icon" />
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,25 +5,25 @@ import { sidebarLinks } from '~/mock/data'
 
 const Sidebar: React.FC = () => {
   return (
-    <aside className="flex flex-col justify-between max-w-[300px] min-h-screen h-full w-full py-8 px-12">
+    <aside className="top-0 flex flex-col items-center md:items-start justify-between max-w-[60px] md:max-w-[300px] min-h-screen h-full w-full py-8 px-0 md:px-2 lg:px-12">
       <div className="flex flex-col space-y-12">
-        <Logo className="w-24 h-14" />
+        <Logo className="w-8 h-8 md:w-24 md:h-14" />
         <div className="flex flex-col space-y-10">
-          <h1 className="text-black text-2xl font-bold">Explore</h1>
+          <h1 className="text-black text-block md:text-2xl font-bold hidden md:block">
+            Explore
+          </h1>
           <nav>
             <ul className="space-y-6">
               {sidebarLinks.map(({ icon, title }, i) => (
                 <li key={i}>
                   <a
                     href="#"
-                    className="flex items-center space-x-4 hover:underline transition ease-in-out duration-150"
+                    className="flex items-center flex-shrink-0 flex-none space-x-4 hover:underline transition ease-in-out duration-150"
                   >
-                    <div className="relative w-6 h-6">
-                      <Image src={icon} layout="fill" alt={icon} />
-                    </div>
-                    <span className="text-black font-semibold text-sm">
+                    <Image src={icon} width={25} height={25} alt={icon} />
+                    <h1 className="text-black font-semibold text-sm hidden md:block">
                       {title}
-                    </span>
+                    </h1>
                   </a>
                 </li>
               ))}
@@ -32,12 +32,12 @@ const Sidebar: React.FC = () => {
         </div>
       </div>
       <div className="flex items-center space-x-3">
-        <div className="inline-flex items-center justify-center p-2 bg-white rounded-full">
+        <div className="inline-flex flex-none flex-shrink-0 items-center justify-center p-2 bg-white rounded-full">
           <Image src="/svgs/message.svg" width={23} height={22} alt="message" />
         </div>
         <a
           href="#"
-          className="text-black font-medium text-sm hover:underline transition ease-in-out duration-150"
+          className="text-black font-medium text-sm hover:underline transition ease-in-out duration-150 hidden md:block"
         >
           Help Center
         </a>
